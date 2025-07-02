568ff50ed405ce02c3a109e175990d5ce989a7af
timicienio <timicienio@25sprout.com>
2025-07-03 03:03:50 +0800

    remove liscence

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -54,7 +54,7 @@ export default function Footer() {
         </li>
       </ul>
       <p className="mt-8 text-neutral-600 dark:text-neutral-300">
-        © {new Date().getFullYear()} MIT Licensed
+        © {new Date().getFullYear()} All Rights Reserved.
       </p>
     </footer>
   );
